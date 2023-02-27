b9c622454f9c7be611da8b3ddd1488c526289a59
Khairul Hasib <khairul@xpeedstudio.com>
2023-02-27 16:14:21 +0600

    utility package naming convention updated, need to check composer json (mmn-stability and req naming))

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
 # utility-package
+
+## Over View of package 
+
+* Wpmet Plugin Utility helper package for Banner , Notice , Stories and Rating.
+* We made a composer package for these classes together.
+* By using this Helper package we are centralizing the feature of these Classes in a package manager
+  (so that any update or modifications of this functionalities globally no need to give hand or keep any dependencies on our plugins)
+
+
+
+## Composer and making :
+
+composer is a php dependency manager which take cares of all dependencies of projects ,
+it take care of namespaces initializing and calling by mapping it in its autoload.php file we just need to specify the root dir in its jeson file call composer.json we can also configure its dependencies here and its author and package name etc. 
+
+``composer.json  file example``
+
+```php 
+{
+    "name": "wpmet/utility-package",
+    "description": "Utility packages for plugin, It will be used to serve Stories, Notice , Banner and Ratings",
+    "type": "library",
+    "license": "GPL",
+    "autoload": {
+        "psr-4": {
+            "Wpmet\\UtilityPackage\\": "src/"
+        }
+    },
+    "authors": [
+        {
+            "name": "XpeedStudio",
+            "email": "info@xpeedstudio.com"
+        },
+        {
+            "name": "Wpmet",
+            "email": "info@xpeedstudio.com"
+        }
+    ],
+    "minimum-stability": "stable",
+    "require": {
+         "php": ">=7.4",
+         "vendor/package": ">=1.0.1"
+
+    },
+    "require-dev": {
+
+    }
+
+}
+```
+### explanation 
+
+* namespace "Wpmet\\UtilityPackage\\" point to the src/ folder directly by composer when it calls 
+* require if any other package needed for this project.
+* required-dev if any other package needed for this project for only development purpose.
+
+
+## to install this package by CLI
+
+`` composer require xs-khairul/pac04  ``
+
+## Using feature example 
+
+```php 
+
+/**
+			 * Show WPMET stories widget in dashboard
+			 */
+			\Wpmet\UtilityPackage\Stories\Stories::instance( 'elementskit-lite' )
+			// ->is_test(true)
+			->set_filter( $filter_string )
+			->set_plugin( 'ElementsKit', 'https://wpmet.com/plugin/elementskit/' )
+			->set_api_url( 'https://api.wpmet.com/public/stories/' )
+			->call();
+
+
+			/**
+			 * Show WPMET banner (codename: jhanda)
+			 */
+			\Wpmet\UtilityPackage\Banner\Banner::instance( 'elementskit-lite' )
+			// ->is_test(true)
+			->set_filter( ltrim( $filter_string, ',' ) )
+			->set_api_url( 'https://api.wpmet.com/public/jhanda' )
+			->set_plugin_screens( 'edit-elementskit_template' )
+			->set_plugin_screens( 'toplevel_page_elementskit' )
+			->call();
+
+
+```
+
---
+++ src/Helper/Helper.php
@@ -0,0 +1,226 @@
+<?php
+namespace Wpmet\UtilityPackage\Helper;
+
+defined('ABSPATH') || exit;
+/**
+ * package helper class.
+ *
+ * @since 1.0.0
+ */
+
+class Helper
+{
+   
+    public static function kses($raw)
+    {
+
+        $allowed_tags = array(
+            'a'                             => array(
+                'class'  => array(),
+                'href'   => array(),
+                'rel'    => array(),
+                'title'  => array(),
+                'target' => array(),
+            ),
+            'abbr'                          => array(
+                'title' => array(),
+            ),
+            'b'                             => array(),
+            'blockquote'                    => array(
+                'cite' => array(),
+            ),
+            'cite'                          => array(
+                'title' => array(),
+            ),
+            'code'                          => array(),
+            'del'                           => array(
+                'datetime' => array(),
+                'title'    => array(),
+            ),
+            'dd'                            => array(),
+            'div'                           => array(
+                'class' => array(),
+                'title' => array(),
+                'style' => array(),
+            ),
+            'dl'                            => array(),
+            'dt'                            => array(),
+            'em'                            => array(),
+            'h1'                            => array(
+                'class' => array(),
+            ),
+            'h2'                            => array(
+                'class' => array(),
+            ),
+            'h3'                            => array(
+                'class' => array(),
+            ),
+            'h4'                            => array(
+                'class' => array(),
+            ),
+            'h5'                            => array(
+                'class' => array(),
+            ),
+            'h6'                            => array(
+                'class' => array(),
+            ),
+            'i'                             => array(
+                'class' => array(),
+            ),
+            'img'                           => array(
+                'alt'    => array(),
+                'class'  => array(),
+                'height' => array(),
+                'src'    => array(),
+                'width'  => array(),
+            ),
+            'li'                            => array(
+                'class' => array(),
+            ),
+            'ol'                            => array(
+                'class' => array(),
+            ),
+            'p'                             => array(
+                'class' => array(),
+            ),
+            'q'                             => array(
+                'cite'  => array(),
+                'title' => array(),
+            ),
+            'span'                          => array(
+                'class' => array(),
+                'title' => array(),
+                'style' => array(),
+            ),
+            'iframe'                        => array(
+                'width'       => array(),
+                'height'      => array(),
+                'scrolling'   => array(),
+                'frameborder' => array(),
+                'allow'       => array(),
+                'src'         => array(),
+            ),
+            'strike'                        => array(),
+            'br'                            => array(),
+            'strong'                        => array(),
+            'data-wow-duration'             => array(),
+            'data-wow-delay'                => array(),
+            'data-wallpaper-options'        => array(),
+            'data-stellar-background-ratio' => array(),
+            'ul'                            => array(
+                'class' => array(),
+            ),
+        );
+
+        if (function_exists('wp_kses')) {
+            // WP is here
+            return wp_kses($raw, $allowed_tags);
+        } else {
+            return $raw;
+        }
+    }
+    public static function get_kses_array()
+    {
+
+        $allowed_tags = array(
+            'a'                             => array(
+                'class'  => array(),
+                'href'   => array(),
+                'rel'    => array(),
+                'title'  => array(),
+                'target' => array(),
+            ),
+            'abbr'                          => array(
+                'title' => array(),
+            ),
+            'b'                             => array(),
+            'blockquote'                    => array(
+                'cite' => array(),
+            ),
+            'cite'                          => array(
+                'title' => array(),
+            ),
+            'code'                          => array(),
+            'del'                           => array(
+                'datetime' => array(),
+                'title'    => array(),
+            ),
+            'dd'                            => array(),
+            'div'                           => array(
+                'class' => array(),
+                'title' => array(),
+                'style' => array(),
+            ),
+            'dl'                            => array(),
+            'dt'                            => array(),
+            'em'                            => array(),
+            'h1'                            => array(
+                'class' => array(),
+            ),
+            'h2'                            => array(
+                'class' => array(),
+            ),
+            'h3'                            => array(
+                'class' => array(),
+            ),
+            'h4'                            => array(
+                'class' => array(),
+            ),
+            'h5'                            => array(
+                'class' => array(),
+            ),
+            'h6'                            => array(
+                'class' => array(),
+            ),
+            'i'                             => array(
+                'class' => array(),
+            ),
+            'img'                           => array(
+                'alt'    => array(),
+                'class'  => array(),
+                'height' => array(),
+                'src'    => array(),
+                'width'  => array(),
+            ),
+            'li'                            => array(
+                'class' => array(),
+            ),
+            'ol'                            => array(
+                'class' => array(),
+            ),
+            'p'                             => array(
+                'class' => array(),
+            ),
+            'q'                             => array(
+                'cite'  => array(),
+                'title' => array(),
+            ),
+            'span'                          => array(
+                'class' => array(),
+                'title' => array(),
+                'style' => array(),
+            ),
+            'iframe'                        => array(
+                'width'       => array(),
+                'height'      => array(),
+                'scrolling'   => array(),
+                'frameborder' => array(),
+                'allow'       => array(),
+                'src'         => array(),
+            ),
+            'strike'                        => array(),
+            'br'                            => array(),
+            'strong'                        => array(),
+            'data-wow-duration'             => array(),
+            'data-wow-delay'                => array(),
+            'data-wallpaper-options'        => array(),
+            'data-stellar-background-ratio' => array(),
+            'ul'                            => array(
+                'class' => array(),
+            ),
+        );
+
+        return $allowed_tags;
+    }
+
+}
